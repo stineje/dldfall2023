3cd45c4bd06b1f2b54009b9f46064e9f65643087
James E. Stine <stineje@jamess-mac-pro.atrc.okstate.edu>
2023-08-01 13:53:33 -0500

    removal

--- Project/datapath_control/adder.sv
+++
@@ -1,5 +0,0 @@
-module adder (input logic [7:0] a, b, output logic [7:0] sum);
-
-   assign sum = a + b;
-
-endmodule // adder
--- Project/datapath_control/sub.sv
+++
@@ -1,6 +0,0 @@
-module sub (input logic [7:0] a, b, output logic [7:0] sum);
-
-   assign sum = a - b;
-
-endmodule // sub
-
